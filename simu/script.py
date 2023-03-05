b9bc4c49b99b82b010f17e499aa0e8b1426fdfef
Marcos Cervetto <cervettomarcos@gmail.com>
2023-03-05 14:16:46 -0300

    script de simulacion python

---
+++ simu/script.py
@@ -0,0 +1,63 @@
+#!/bin/env python3
+
+from pathlib import Path
+import subprocess
+import sys
+import os
+import stat
+import numpy as np
+
+POWERDEVS_HOME = Path(os.getenv('POWERDEVS_HOME','../../powerdevs/'))
+
+default_tf = 200000
+parameters = {'tc': {'start': 23.2, 'stop': 25, 'step':0.2, 'tf':200000}}
+
+def get_environment():
+    
+    pdppt_path = POWERDEVS_HOME / 'bin' / 'pdppt'
+    if not Path.exists(pdppt_path):
+        print(f'No se puede acceder a pdppt en: {pdppt_path}')    
+        sys.exit()
+
+    os.environ['LD_LIBRARY_PATH'] = str(POWERDEVS_HOME / 'bin')
+
+    return pdppt_path
+
+
+def compilation(source):
+         
+    if not Path.exists(source):
+        print(f'No se puede acceder al model en: {source}')
+        sys.exit()
+
+    target = Path(POWERDEVS_HOME / 'output' / 'model')
+    
+    if not Path(target).exists() or (os.stat(source)[stat.ST_MTIME] > os.stat(target)[stat.ST_MTIME]):
+        subprocess.run([pdppt, '-m', source])
+
+    return target
+
+if __name__ == '__main__':
+    pdppt = get_environment()
+
+    model_path = Path(__file__).parent / '..' / 'circadiano.pdm'
+    compiled = compilation(model_path)
+    model_params = Path(compiled).parent / 'model.params'
+    for parameter, values in parameters.items():
+        start = values['start']
+        stop = values['stop']
+        step = values['step']
+        tf = values.get('tf',default_tf)
+        
+        print(f"Simulando {parameter} desde {start} -> {stop} ({step}) ..")
+        for value in np.arange(start,stop,step):
+            print(f'{parameter}:{value:.3f}')
+            subprocess.run([compiled, '--c', str(model_params),
+                            '-tf', str(tf), 
+                            f'-{parameter}', str(value)])
+
+            # INSERTAR INVOCACION A PROCESAMIENTO DE RESULTADO CON R
+
+
+    
+    
