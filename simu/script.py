85ed2724fa273affcde7e5ed1f0301efbf5a723d
Marcos Cervetto <cervettomarcos@gmail.com>
2023-03-05 15:44:35 -0300

    output con diferente filename

--- simu/script.py
+++ simu/script.py
@@ -5,12 +5,13 @@ import subprocess
 import sys
 import os
 import stat
+import shutil
 import numpy as np
 
 POWERDEVS_HOME = Path(os.getenv('POWERDEVS_HOME','../../powerdevs/'))
 
 default_tf = 200000
-parameters = {'tc': {'start': 23.2, 'stop': 25, 'step':0.2, 'tf':200000}}
+parameters = {'tc': {'start': 23.2, 'stop': 25, 'step':0.2, 'tf':200000, 'output_file':'Qm.csv'}}
 
 def get_environment():
     
@@ -41,13 +42,15 @@ if __name__ == '__main__':
     pdppt = get_environment()
 
     model_path = Path(__file__).parent / '..' / 'circadiano.pdm'
-    compiled = compilation(model_path)
+    compiled = compilation(model_path)    
     model_params = Path(compiled).parent / 'model.params'
+
     for parameter, values in parameters.items():
         start = values['start']
         stop = values['stop']
         step = values['step']
         tf = values.get('tf',default_tf)
+        output_file = values['output_file']
         
         print(f"Simulando {parameter} desde {start} -> {stop} ({step}) ..")
         for value in np.arange(start,stop,step):
@@ -55,7 +58,10 @@ if __name__ == '__main__':
             subprocess.run([compiled, '--c', str(model_params),
                             '-tf', str(tf), 
                             f'-{parameter}', str(value)])
-
+            
+            output_file_param = f'{Path(output_file).stem}-{parameter}{value:.3f}{Path(output_file).suffix}'
+            print(f'Salida en: {output_file_param}')
+            shutil.move(output_file, output_file_param)
             # INSERTAR INVOCACION A PROCESAMIENTO DE RESULTADO CON R
 
 
